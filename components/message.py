07d1000a1adb7c489842cea6559380f9f2f06772
Adel Suarez <adel90suarez@gmail.com>
2023-08-25 19:30:11 -0400

    the colors in the messages are implemented according to the type, and a color selector is implemented

--- components/message.py
+++ components/message.py
@@ -2,13 +2,26 @@ from settings import settings
 from view.view import View
 
 class Message(View):
-    def __init__(self, is_view, is_message) -> None:
-        super().__init__(is_view, is_message)
+    def __init__(self, is_view, is_message, type_message) -> None:
+        super().__init__(is_view, is_message, type_message)
+
+        self.message_colors : dict[str : str] = {
+            'error': settings.RED,
+            'warning': settings.YELLOW,
+            'approved': settings.GREEN,
+        }
+
         self.message()
 
     def message(self):
         if self.is_view:
+
+            for type, color in self.message_colors.items():
+                if type == self.type_message:
+                    print(f'{color}', end='')
+                    
             print(self.is_message.center(settings.SPACE, settings.CARACTER))
+            print(f'{settings.RESET}', end='')
 
 
 
