55b32b8d552e4a118d01e9408d08615eb7d99007
Adel Suarez <adel90suarez@gmail.com>
2023-08-25 19:36:05 -0400

    indentation changes

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from db.DataBase import DataBase
 from settings import clear
 
 query = '''CREATE TABLE IF NOT EXISTS CONTACT (
-			ID INTEGER,
-			NAME TEXT NOT NULL, 
-			NUMBER INTEGER NOT NULL,
-			PRIMARY KEY("ID" AUTOINCREMENT)
+				ID INTEGER,
+				NAME TEXT NOT NULL, 
+				NUMBER INTEGER NOT NULL,
+				PRIMARY KEY("ID" AUTOINCREMENT)
 			)'''
 
 if __name__ == '__main__':
