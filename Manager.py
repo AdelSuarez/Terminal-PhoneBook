bd9f6384243f9ffed14f5704f8bd9b38532ecd83
Adel Suarez <adel90suarez@gmail.com>
2023-08-25 19:46:23 -0400

    Change in the imports and the new parameters were added due to the changes in the view class

--- Manager.py
+++ Manager.py
@@ -1,36 +1,28 @@
-from view.Create_contact import CreateContact
-from view.View_contacts import ViewContacts
-from view.Delete_contact import DeleteContact
-from view.Edit_contact import EditContact
-from view import view_main
+from view import Create_contact, View_contacts, Delete_contact, Edit_contact, view_main
 from settings import clear
-
-class View:
-    def __init__(self, is_view, is_message) -> None:
-        self.is_view = is_view
-        self.is_message = is_message
+from view.view import View
 
 class Manager(View):
-    def __init__(self, is_view = False, is_message = '') -> None:
-        super().__init__(is_view, is_message)
+    def __init__(self, is_view = False, is_message = '', type_message = '') -> None:
+        super().__init__(is_view, is_message, type_message)
 
     def manager(self):
 
         while True:
-            self.option = view_main.ViewMain(self.is_view, self.is_message).views_main()
+            self.option = view_main.ViewMain(self.is_view, self.is_message, self.type_message).views_main()
 
             if self.option == 1:
-                CreateContact(self.is_view, self.is_message).view_new_contact()
+                Create_contact.CreateContact(self.is_view, self.is_message, self.type_message).view_new_contact()
 
             elif self.option == 2:
-                ViewContacts().all_contacts()
+                View_contacts.ViewContacts().all_contacts()
 
             elif self.option == 3:
                 clear.Clear()
-                DeleteContact(self.is_view, self.is_message).delete()
+                Delete_contact.DeleteContact(self.is_view, self.is_message, self.type_message).delete()
 
             elif self.option == 4:
-                EditContact(self.is_view, self.is_message).mian_edit()
+                Edit_contact.EditContact(self.is_view, self.is_message, self.type_message).mian_edit()
 
             elif self.option == 0:
                 break
