7fd07315189ee09a83684a3f3102ee145c8e3174
Adel Suarez <adel90suarez@gmail.com>
2023-08-25 19:48:38 -0400

    A new variable was added to be able to control the type of message for the color, and a function was added that controls the variables of the messages.

--- view/view.py
+++ view/view.py
@@ -1,7 +1,8 @@
 class View:
-    def __init__(self, is_view, is_message) -> None:
-        self.is_view = is_view
-        self.is_message = is_message
+    def __init__(self, is_view, is_message, type_message) -> None:
+        self.is_view : bool = is_view
+        self.is_message : str = is_message
+        self.type_message : str = type_message
 
     def view_option_menu(self, options):
         print("+-----------------------+--------------+")
@@ -10,4 +11,10 @@ class View:
             print('| {:<22}| {:<12} |'.format(description,command))
             
         print("+-----------------------+--------------+")
+
+    def message_variables(self, view : bool , message : str, type : str):
+        self.is_view = view
+        self.is_message = message
+        self.type_message = type
+    
         
