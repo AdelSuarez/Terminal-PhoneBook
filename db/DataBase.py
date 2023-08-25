f05ddfa6ab15d4dacfbe61eb3013347aef554719
Adel Suarez <adel90suarez@gmail.com>
2023-08-25 15:48:33 -0400

    The code was documented and changes were made to the queries, where the code was optimized

--- db/DataBase.py
+++ db/DataBase.py
@@ -2,59 +2,48 @@ import sqlite3 as sql
 
 class DataBase:
     def __init__(self, db = 'DataBase.db'):
-        self.database = db
+        self.database : str = db
 
-    def connect_db(self, query, parameters = (),):
+    def connect_db(self, query : str, parameters : tuple = (),):
+       # Make the connection according to he query you want to make  
         with sql.connect(self.database) as conn:
             cur = conn.cursor()
             result =  cur.execute(query, parameters)
             conn.commit()
         return result
 
-    # def fetchall(self):
-        # return self.connect_db().fetchall()
 
-    def create_contact(self, name, number):
-        parameters = (name, number)
-        query = 'INSERT INTO CONTACT VALUES(NULL,?,?)'
-        self.connect_db(query, parameters)
+    # CREATE
+    def create_contact(self, name : str, number : int):
+        self.connect_db('INSERT INTO CONTACT VALUES(NULL,?,?)', (name, number))
 
-    def delete_contact_name(self, parameters):
-        query = 'DELETE FROM CONTACT WHERE name LIKE ?'
-        self.connect_db(query, parameters)
+    # DELETE
+    def delete_contact_name(self, name : str):
+        self.connect_db('DELETE FROM CONTACT WHERE name LIKE ?', (name,))
 
-    def delete_contact_id(self, parameters):
-        query = 'DELETE FROM CONTACT WHERE id=?'
-        self.connect_db(query, parameters)
+    def delete_contact_id(self, id : int):
+        self.connect_db('DELETE FROM CONTACT WHERE id=?', (id,))
 
+    # VIEW
     def all_contacts(self):
-        query = 'SELECT * FROM CONTACT ORDER BY name, id'
-        return  self.connect_db(query, ).fetchall()
+        return  self.connect_db('SELECT * FROM CONTACT ORDER BY name, id', ).fetchall()
 
-    def search_name_db(self, name):
-        parameters = (name, )
-        query = 'SELECT * FROM CONTACT WHERE name LIKE ?'
-        return self.connect_db(query, parameters).fetchall()
+    # SEARCH
+    def search_name_db(self, name : str):
+        return self.connect_db('SELECT * FROM CONTACT WHERE name LIKE ?', (name, )).fetchall()
     
-    def search_id_db(self, name):
-        parameters = (name, )
-        query = 'SELECT * FROM CONTACT WHERE id=?'
-        return self.connect_db(query, parameters).fetchall()
+    def search_id_db(self, name : str):
+        return self.connect_db('SELECT * FROM CONTACT WHERE id=?', (name, )).fetchall()
 
-    def update_name(self, parameters):
-        query = 'UPDATE CONTACT SET name = ? WHERE name = ?'
-        self.connect_db(query, parameters)
+    # UPDATE
+    def update_name(self, new_name : str, name : str):
+        self.connect_db('UPDATE CONTACT SET name = ? WHERE name = ?', (new_name, name))
 
-    def update_name_id(self, parameters):
-        query = 'UPDATE CONTACT SET name = ? WHERE id = ?'
-        self.connect_db(query, parameters)
+    def update_name_id(self, new_name : str, id : int):
+        self.connect_db('UPDATE CONTACT SET name = ? WHERE id = ?', (new_name, id))
 
-    def update_number(self, parameters):
-        query = 'UPDATE CONTACT SET number = ? WHERE name = ?'
-        self.connect_db(query, parameters)
+    def update_number(self, new_number : int, name : str):
+        self.connect_db('UPDATE CONTACT SET number = ? WHERE name = ?', (new_number, name))
 
     def update_number_id(self, parameters):
-        query = 'UPDATE CONTACT SET number = ? WHERE id = ?'
-        self.connect_db(query, parameters)
-
-
+        self.connect_db('UPDATE CONTACT SET number = ? WHERE id = ?', parameters)
