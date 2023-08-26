af564fc674a560c9543d73880ad6f2b25d5a90b1
Adel Suarez <adel90suarez@gmail.com>
2023-08-25 21:20:09 -0400

    implementation of inheritance of the view class, and use of its properties

--- view/View_contacts.py
+++ view/View_contacts.py
@@ -1,7 +1,11 @@
 from settings import settings, clear
 from db.DataBase import DataBase as db
+from . import view
+from components import message
 
-class ViewContacts:
+class ViewContacts(view.View):
+	def __init__(self, is_view, is_message, type_message) -> None:
+		super().__init__(is_view, is_message, type_message)
 
 	def all_contacts(self):
 		clear.Clear()
@@ -9,6 +13,7 @@ class ViewContacts:
 			all_contacts = db().all_contacts()
 				
 			try:
+				
 				if all_contacts != []:
 					self.view_all_contacts( all_contacts)
 
@@ -17,19 +22,23 @@ class ViewContacts:
 
 				go_back = int(input('| 0 para regresar >> '))
 				if go_back != 0:
+
 					clear.Clear()
-					print('Opción incorrecta'.center(settings.SPACE, settings.CARACTER))
+					self.message_variables(True, 'Opción incorrecta', 'warning')
 					continue
+
 				clear.Clear()
 				break
 
 			except ValueError:
 				clear.Clear()
-				print('Introduce solo números'.center(settings.SPACE, settings.CARACTER))
+				self.message_variables(True, 'Introduce solo números', 'error')
+
 
 
 
 	def view_all_contacts(self, database ):
+		message.Message(self.is_view, self.is_message, self.type_message)
 		print("+--------------------------------------+")
 		print("|               AGENDA                 |")
 		print("+----+--------------------+------------+")
