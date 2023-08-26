8316287ee64e915808c36fed144260d6c4f5bd2c
Adel Suarez <adel90suarez@gmail.com>
2023-08-25 20:05:03 -0400

    The changes that were made to the view class were added, the new parameter and the function that controls the variables of the message

--- view/view_main.py
+++ view/view_main.py
@@ -1,13 +1,13 @@
-from settings import settings, clear
+from settings import clear
 from components import message
 from . import view
 
 class ViewMain(view.View):
 
-    def __init__(self, is_view, is_message) -> None:
-        super().__init__(is_view, is_message)
+    def __init__(self, is_view, is_message, type_message) -> None:
+        super().__init__(is_view, is_message, type_message)
         
-        self.view_options = {
+        self.view_options : dict[str : str] = {
             'Presione 1': 'Nuevo contacto',
             'Presione 2': 'Ver contactos',
             'Presione 3': 'Borrar contacto',
@@ -15,35 +15,37 @@ class ViewMain(view.View):
             'Presione 0': 'Salir'
         }
 
-    def views_main(self):
+    def views_main(self) -> int:
 
         while True:
-            if not ViewOptions(self.is_view, self.is_message, self.view_options).view_options():
+            if not ViewOptions(self.is_view, self.is_message,self.type_message, self.view_options).view_options():
                 self.is_view = False
                 
             try:
-                self.option = int(input('| Selecciona una opción >> '))
-                if not(0 <= self.option < len(self.view_options)):
-                    self.is_view = True
-                    self.is_message = 'La opcion no existe'
+                option = int(input('| Selecciona una opción >> '))
+                if not(0 <= option < len(self.view_options)):
+
+                    self.message_variables(True, 'La opcion no existe', 'warning')
                     clear.Clear()
+
                     continue
-                return self.option
+
+                self.type_message = ''
+                return option
 
             except ValueError:
-                self.is_view = True
-                self.is_message = 'Introduce solo números'
-                print('Introduce solo números'.center(settings.SPACE, settings.CARACTER))
+                
+                self.message_variables(True, 'Introduce solo números', 'error')
                 clear.Clear()
 
         
 class ViewOptions(view.View):
-    def __init__(self, is_view, is_message, options) -> None:
-        super().__init__(is_view, is_message)
+    def __init__(self, is_view, is_message, type_message, options) -> None:
+        super().__init__(is_view, is_message, type_message)
         self.options = options
 
-    def view_options(self) -> None:
-        message.Message(self.is_view, self.is_message)
+    def view_options(self) -> bool:
+        message.Message(self.is_view, self.is_message, self.type_message)
         print("+--------------------------------------+")
         print('|          AGENDA DE CONTACTOS         |')
         print('|----------------INICIO----------------|')
